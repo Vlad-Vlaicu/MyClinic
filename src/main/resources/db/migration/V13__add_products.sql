8fe097bca15e882928ed2b417db6866ff937dc3a
Vlad-Vlaicu <vlad_gabriel.vlaicu@stud.acs.upb.ro>
2024-03-26 09:22:16 +0200

    added some products for testing

---
+++ src/main/resources/db/migration/V13__add_products.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.product_info (id, name, description, price, category)
+VALUES
+    (1, 'Product 1', 'Description of Product 1', 10.99, 'CARDIOLOGIE'),
+    (2, 'Product 2', 'Description of Product 2', 15.99, 'ONCOLOGIE'),
+    (3, 'Product 3', 'Description of Product 3', 20.99, 'CARDIOLOGIE A'),
+    (4, 'Product 4', 'Description of Product 4', 25.99, 'RADIOLOGIE');
