ff8ae2ae39e00d51c45b4cd80d5759e71d422a5b
Vlad-Vlaicu <vlad_gabriel.vlaicu@stud.acs.upb.ro>
2024-03-26 08:38:08 +0200

    tested functionalities and repaired paymentMapper + added postman collection

--- src/main/java/com/pweb/MyClinic/mappers/PaymentInfoMapper.java
+++ src/main/java/com/pweb/MyClinic/mappers/PaymentInfoMapper.java
@@ -2,6 +2,7 @@ package com.pweb.MyClinic.mappers;
 
 import com.pweb.MyClinic.model.db.PaymentInfo;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -9,7 +10,8 @@ public interface PaymentInfoMapper {
 
     PaymentInfoMapper INSTANCE = Mappers.getMapper(PaymentInfoMapper.class);
 
-    PaymentInfo paymentInfoGenToPaymentInfoDB(com.pweb.model.generated.PaymentInfo source);
-
+    @Mapping(source = "executionTime", target = "executionDate")
+    @Mapping(source = "product.id", target = "productId")
+    @Mapping(source = "status", target = "paymentStatus")
     com.pweb.model.generated.PaymentInfo paymentInfoDBToPaymentInfoGen(PaymentInfo source);
 }
